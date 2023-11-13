9ed8a836f4d85d9367abd570a83e1d23e9b59ac1
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-11-12 17:01:37 -0800

    completed models index, category-routes, and almost finished with product routes

--- models/index.js
+++ models/index.js
@@ -7,10 +7,29 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: {
+    name: "category_id",
+    allowNull: false
+  }
+});
+Product.belongsTo(Category)
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: falses
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,40 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = Category.findAll(req.body, {
+      include: [
+        { model: Tag, through: ProductTag, as: 'product_tag' },
+        {model: Category, through: Product.category_id, as:'product_id'}
+      ]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = category.findByPk(req.params.id, {
+      include: [
+        { model: Tag, through: ProductTag, as: 'product_tag' },
+        {model: Category, through: Product.category_id, as:'product_id'}
+      ]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
